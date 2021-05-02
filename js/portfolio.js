7a1380b3e24498674eb933363e8d82fa72e078a9
Pablo Djordjevic <pablodjordjevic@gmail.com>
2021-05-01 22:14:00 -0300

    bootstrap+sass

---
+++ js/portfolio.js
@@ -0,0 +1,18 @@
+// SCRIPT PORTFOLIO
+$('.categories-portfolio .btn').click(function(){
+  $('.categories-portfolio .btn').removeClass('active');
+  $(this).addClass('active');
+  
+  var data = $(this).attr('data-filter');
+  $grid.isotope({
+    filter: data
+  })
+});
+
+var $grid = $(".grid").isotope({
+  itemSelector: ".all",
+  percentPosition: true,
+  masonry: {
+    columnWidth: ".all"
+  }
+})
